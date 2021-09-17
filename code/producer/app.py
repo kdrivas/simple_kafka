398e78f54ad1d6ac2caecafbf16131607a7a0255
kervyRivas <a20112128@pucp.pe>
2021-09-17 16:50:04 -0500

    adding routes

--- code/producer/app.py
+++ code/producer/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
@@ -6,9 +6,17 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
+messages = ['hola', 'mundo']
+
 @app.get('/', response_class=HTMLResponse)
 def index(request: Request):
-  return templates.TemplateResponse("producer.html", {"request": request})
+  print('get')
+  return templates.TemplateResponse("producer.html", {"request": request, "messages": messages})
+
+@app.post('/', response_class=HTMLResponse)
+def submitMessage(request: Request, message: str=Form(...)):
+  print('adasd', message)
+  return templates.TemplateResponse("producer.html", {"request": request, "messages": messages})
 
 
 
