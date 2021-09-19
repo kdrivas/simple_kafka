672ee45dce7669c79daff5f2223ff548428346a7
kervyRivas <a20112128@pucp.pe>
2021-09-18 23:27:54 -0500

    setting kafka conn producer

--- code/producer/app.py
+++ code/producer/app.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from kafka import KafkaProducer
+
+import os
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
-
 messages = ['hola', 'mundo']
+
+KAFKA_SERVER = os.getenv('KAFKA_SERVER', 'kafka')
+KAFKA_PORT = os.getenv('KAFKA_SERVER', 'kafka')
+KAFKA_CONN = KAFKA_SERVER + ":" + KAFKA_PORT
 
 @app.get('/', response_class=HTMLResponse)
 def index(request: Request):
